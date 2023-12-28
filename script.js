01f73e6c6d13f8e0ca0a145f0b19ea3ce494b62c
xMatuszek <45698169+xMatuszek@users.noreply.github.com>
2023-12-28 17:16:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,77 +3,76 @@ const openaiApiKey = process.env.OPENAI_API_KEY;
 
 if (!openaiApiKey) {
     console.error("Nie ustawiono klucza API OpenAI.");
-}
-else{
+} else {
+    // Sprawdź, czy jesteśmy w środowisku przeglądarki
+    if (typeof document !== 'undefined') {
+        // Kod dla przeglądarki
+        document.getElementById('submit-btn').addEventListener('click', function () {
+            const userInput = document.getElementById('user-input').value;
+            const userInstruction = document.getElementById('user-instruction').value;
 
-
-document.getElementById('submit-btn').addEventListener('click', function() {
-    const userInput = document.getElementById('user-input').value;
-    const userInstruction = document.getElementById('user-instruction').value;
-
-    fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${openaiApiKey}`
-        },
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'system',
-                    content: userInstruction
+            fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${openaiApiKey}`
                 },
-                {
-                    role: 'user',
-                    content: userInput
-                }
-            ]
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        let diffText = '';
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [
+                        {
+                            role: 'system',
+                            content: userInstruction
+                        },
+                        {
+                            role: 'user',
+                            content: userInput
+                        }
+                    ]
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    let diffText = '';
 
-        const userInput = document.getElementById('user-input').value.split(' ');
-        const gptCorrectedText = data.choices[0].message.content;
-        const correctedText = gptCorrectedText.split(' ');
+                    const userInput = document.getElementById('user-input').value.split(' ');
+                    const gptCorrectedText = data.choices[0].message.content;
+                    const correctedText = gptCorrectedText.split(' ');
 
-        // The user input should not be cleared after the submit button is clicked
-
-        let i = 0, j = 0;
-        while (i < userInput.length - 1 || j < correctedText.length - 1) {
-            if (userInput[i] === correctedText[j]) {
-                diffText += userInput[i] + ' ';
-                i++;
-                j++;
-            } else if (i < userInput.length - 1 && j < correctedText.length - 1) {
-                if (userInput[i] !== correctedText[j]) {
-                    diffText += '<del>' + userInput[i] + '</del> ';
-                    i++;
-                    if (userInput[i] !== correctedText[j]) {
-                        diffText += '<ins>' + correctedText[j] + '</ins> ';
-                        j++;
+                    let i = 0, j = 0;
+                    while (i < userInput.length - 1 || j < correctedText.length - 1) {
+                        if (userInput[i] === correctedText[j]) {
+                            diffText += userInput[i] + ' ';
+                            i++;
+                            j++;
+                        } else if (i < userInput.length - 1 && j < correctedText.length - 1) {
+                            if (userInput[i] !== correctedText[j]) {
+                                diffText += '<del>' + userInput[i] + '</del> ';
+                                i++;
+                                if (userInput[i] !== correctedText[j]) {
+                                    diffText += '<ins>' + correctedText[j] + '</ins> ';
+                                    j++;
+                                }
+                            }
+                        } else if (i < userInput.length - 1) {
+                            diffText += '<del>' + userInput[i] + '</del> ';
+                            i++;
+                        } else if (j < correctedText.length - 1) {
+                            diffText += '<ins>' + correctedText[j] + '</ins> ';
+                            j++;
+                        }
                     }
-                }
-            } else if (i < userInput.length - 1) {
-                diffText += '<del>' + userInput[i] + '</del> ';
-                i++;
-            } else if (j < correctedText.length - 1) {
-                diffText += '<ins>' + correctedText[j] + '</ins> ';
-                j++;
-            }
-        }
 
-        if (userInput[i] === correctedText[j].slice(0, -1)) {
-            diffText += userInput[i] + '. ';
-        } else {
-            diffText += '<del>' + userInput[i] + '</del> ';
-            diffText += '<ins>' + correctedText[j] + '</ins> ';
-        }
+                    if (userInput[i] === correctedText[j].slice(0, -1)) {
+                        diffText += userInput[i] + '. ';
+                    } else {
+                        diffText += '<del>' + userInput[i] + '</del> ';
+                        diffText += '<ins>' + correctedText[j] + '</ins> ';
+                    }
 
-        document.getElementById('corrected-text').innerHTML = diffText;
-    })
-    .catch(error => console.error('Error:', error));
-}); }
+                    document.getElementById('corrected-text').innerHTML = diffText;
+                })
+                .catch(error => console.error('Error:', error));
+        });
+    }
+}
